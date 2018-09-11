b8b416436759c9f2af9d0ec94f7531ebe9d5c18c
Dawid Ba <dawid.babis@gmail.com>
2018-09-11 17:28:39 +0200

    Entry entities for loading coordinates from DB

---
+++ src/Entity/Coordinates.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CoordinatesRepository")
+ */
+class Coordinates
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $route_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lat;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lng;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRouteId(): ?int
+    {
+        return $this->route_id;
+    }
+
+    public function setRouteId(int $route_id): self
+    {
+        $this->route_id = $route_id;
+
+        return $this;
+    }
+
+    public function getLat(): ?string
+    {
+        return $this->lat;
+    }
+
+    public function setLat(string $lat): self
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getLng(): ?string
+    {
+        return $this->lng;
+    }
+
+    public function setLng(string $lng): self
+    {
+        $this->lng = $lng;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+}
