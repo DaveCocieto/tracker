3279913955138b9a79fde2a0c0713150e75d2689
Dawid Ba <dawid.babis@gmail.com>
2018-09-12 14:27:02 +0200

    Add more sample routes to var

--- src/Command/SampleRoutesCommand.php
+++ src/Command/SampleRoutesCommand.php
@@ -12,9 +12,9 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class SampleRoutesCommand extends Command {
 
     private $routes = [
-        'Route1',
-        //'Route2',
-        //'Route3'
+        0 => 'Route1',
+        1 => 'Route2',
+        //2 => 'Route3'
     ];
 
     private $coordinates = [
@@ -38,6 +38,103 @@ class SampleRoutesCommand extends Command {
                 'lat' => '50.267341',
                 'lng' => '18.588219',
                 'route_id' => 1
+            ],
+            [
+                'lat' => '50.26339',
+                'lng' => '18.615685',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.260757',
+                'lng' => '18.64109',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.259001',
+                'lng' => '18.671646',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.258781',
+                'lng' => '18.704262',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.260537',
+                'lng' => '18.721428',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.238803',
+                'lng' => '18.708725',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.227164',
+                'lng' => '18.697395',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.214862',
+                'lng' => '18.691902',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.205634',
+                'lng' => '18.697052',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.198822',
+                'lng' => '18.699799',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.184974',
+                'lng' => '18.706665',
+                'route_id' => 1
+            ],
+            [
+                'lat' => '50.173981',
+                'lng' => '18.716621',
+                'route_id' => 1
+            ]
+        ],
+        1 => [
+            [
+                'lat' => '50.084717',
+                'lng' => '18.66384',
+                'route_id' => 2
+            ],
+            [
+                'lat' => '50.084717',
+                'lng' => '18.66384',
+                'route_id' => 2
+            ],
+            [
+                'lat' => '50.075794',
+                'lng' => '18.664184',
+                'route_id' => 2
+            ],
+            [
+                'lat' => '50.070616',
+                'lng' => '18.663497',
+                'route_id' => 2
+            ],
+            [
+                'lat' => '50.068302',
+                'lng' => '18.66178',
+                'route_id' => 2
+            ],
+            [
+                'lat' => '50.070065',
+                'lng' => '18.664527',
+                'route_id' => 2
+            ],
+            [
+                'lat' => '50.067971',
+                'lng' => '18.664355',
+                'route_id' => 2
             ],
         ]
     ];
