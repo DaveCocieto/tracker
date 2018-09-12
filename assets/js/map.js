a2e4e080ba36a87c4fce93d6aa95ba639e79c860
Dawid Ba <dawid.babis@gmail.com>
2018-09-12 22:44:09 +0200

    Add js functions to operate with fetched json data Added feature to clear map of polyline layers and put fetched from DB coordinates to map

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,25 +2,6 @@ require('../scss/map.scss');
 
 console.log('MAP INIT');
 
-var loadRouteToMap = function(data) {
-    //console.log(data);
-    //console.log(data.target);
-    var element = $(data.target);
-    var route_id = element.data('route-id')
-    console.log(route_id);
-    //alert('load route');
-
-    $.ajax({
-      /*method: "POST",
-      url: "some.php",
-      data: { name: "John", location: "Boston" }*/
-      method: 'GET',
-      url: '/get_route/' + route_id
-    }).done(function(data) {
-      console.log(data);
-    });
-};
-
 tomtom.setProductInfo('tracker', '0.1');
 var map = tomtom.map('map', {
     key: 'key',
@@ -44,7 +25,7 @@ tomtom.controlPanel({
     .addContent(languageLabel);
 
 // polygons
-var polygon = {
+/*var polygon = {
     'type': 'FeatureCollection',
     'features': [
         {
@@ -66,8 +47,67 @@ var polygon = {
     ]
 };
 var geoJson = tomtom.L.geoJson(polygon, { style: { color: '#00d7ff', opacity: 0.8 } }).addTo(map);
-map.fitBounds(geoJson.getBounds(), { padding: [5, 5] });
+map.fitBounds(geoJson.getBounds(), { padding: [5, 5] });*/
+
+var clearMap = function() {
+    map.eachLayer(function (layer) {
+        /*
+         * Sprawdzamy czy parametry warstwy sa okreslone
+         * Sprawdzamy typ geomtrii, jezeli to MultiLineString (czyli polyline) to usuwamy
+         */
+        if (layer.feature != undefined && layer.feature.geometry != undefined && layer.feature.geometry.type === 'MultiLineString') {
+            map.removeLayer(layer);
+        }
+    });
+};
+
+var loadRouteToMap = function(json) {
+    var coordintes = [];
+
+    for (i = 0; i < json.length; i++) {
+        coordintes.push([
+            json[i].lng,
+            json[i].lat
+        ]);
+    }
+
+    var polyline = {
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': 'Feature',
+                'properties': {},
+                'geometry': {
+                    //'type': 'Polygon',
+                    'type': 'MultiLineString',
+                    'coordinates': [
+                        coordintes
+                    ]
+                }
+            }
+        ]
+    };
+
+    clearMap();
+    var geoJson = tomtom.L.geoJson(polyline, { style: { color: '#00d7ff', opacity: 0.8 } }).addTo(map);
+    map.fitBounds(geoJson.getBounds(), { padding: [5, 5] });
+};
+
+var loadRouteFromDB = function(data) {
+    var element = $(data.target);
+    var route_id = element.data('route-id')
+
+    $.ajax({
+      /*method: "POST",
+      url: "some.php",
+      data: { name: "John", location: "Boston" }*/
+      method: 'GET',
+      url: '/get_route/' + route_id
+    }).done(function(data) {
+      loadRouteToMap(data);
+    });
+};
 
 $('.route-item').click(function(e) {
-    loadRouteToMap(e);
+    loadRouteFromDB(e);
 });
